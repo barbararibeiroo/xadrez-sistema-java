f0223bc0fae465183670dd8b7c8ea87ea49f8606
Bárbara <barbararibeiro18@gmail.com>
2020-08-19 23:34:06 -0300

    Primeira Classe: Posicao

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 
 	}
 
